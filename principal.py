7c50e825f4ca2c01e094a08c75cc21c6cdb8458d
Damien-Collot <dcollot76@gmail.com>
2023-10-06 17:42:20 +0200

    Feat : watdogs de wish (merci nico)
    Co-authored-by: Nicolas-wrobel <Nicolas-wrobel@users.noreply.github.com>

--- principal.py
+++ principal.py
@@ -1,9 +1,11 @@
 import os
 import time
+import socket
 import multiprocessing
-import signal
 from secondaire import serveur_secondaire
 
+HOST = '127.0.0.1'
+PORT = 65432
 PATH_TO_DWTUBE = "dwtube1"
 PATH_TO_WDTUBE = "wdtube1"
 NUM_EXCHANGES = 10
@@ -14,11 +16,25 @@ if not os.path.exists(PATH_TO_DWTUBE):
 if not os.path.exists(PATH_TO_WDTUBE):
     os.mkfifo(PATH_TO_WDTUBE)
 
-def handle_check_signal(signum, frame):
-    # Envoie une réponse au watchdog
-    os.kill(os.getppid(), signal.SIGUSR2)
+def connect_to_watchdog():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        while True:
+            try:
+                data = s.recv(1024)
+                if not data:
+                    break
+                if data == b'ping':
+                    s.sendall(b'pong')
+            except ConnectionResetError:
+                print("La connexion avec le watchdog a été interrompue.")
+                break
+
 
 def serveur_principal(valeur_shared):
+    p_watchdog = multiprocessing.Process(target=connect_to_watchdog)
+    p_watchdog.start()
+
     with open(PATH_TO_DWTUBE, "w") as dwtube, open(PATH_TO_WDTUBE, "r") as wdtube:
         for _ in range(NUM_EXCHANGES):
             valeur_shared.value += 1
@@ -27,19 +43,27 @@ def serveur_principal(valeur_shared):
             print("Serveur principal: Envoi de 'ping'")
             dwtube.write("ping\n")
             dwtube.flush()
+            print("Serveur principal: 'ping' envoyé.")
 
-            # Attendre une réponse
+            print("Serveur principal: Attente de la réponse ...")
             response = wdtube.readline().strip()
             print(f"Serveur principal: Reçu '{response}'")
 
             time.sleep(1)
 
-    os.remove(PATH_TO_DWTUBE)
-    os.remove(PATH_TO_WDTUBE)
+    if os.path.exists(PATH_TO_DWTUBE):
+        os.remove(PATH_TO_DWTUBE)
+
+    if os.path.exists(PATH_TO_WDTUBE):
+        os.remove(PATH_TO_WDTUBE)
+
+    p_watchdog.terminate()
+    p_watchdog.join()
+
+
 
 if __name__ == '__main__':
     valeur_shared = multiprocessing.Value('i', 0)
-    signal.signal(signal.SIGUSR1, handle_check_signal)
 
     # Lancement du serveur secondaire avec fork
     p = multiprocessing.Process(target=serveur_secondaire, args=(valeur_shared,))
--- watchdog.py
+++ watchdog.py
@@ -1,36 +1,40 @@
-import signal
+import socket
 import time
-import os
 import multiprocessing
 from principal import serveur_principal
 
-CHECK_INTERVAL = 5  # Vérifie chaque 5 secondes
-RESPONSE_TIMEOUT = 3  # Attends 3 secondes pour une réponse
-process_to_check = None
+HOST = '127.0.0.1'
+PORT = 65432
+CHECK_INTERVAL = 1
+RESPONSE_TIMEOUT = 3
 
-def send_check_signal(signum, frame):
-    global process_to_check
-    if process_to_check:
-        os.kill(process_to_check.pid, signal.SIGUSR1)
+def watchdog_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        print("Watchdog: Écoute des connexions...")
+        conn, addr = s.accept()
+        with conn:
+            print(f"Watchdog: Connecté à {addr}")
+            while True:
+                conn.sendall(b'ping')
+                print("Watchdog: Ping envoyé. Attente du Pong...")
+                try:
+                    data = conn.recv(1024)
+                except ConnectionResetError:
+                    print("Watchdog: La connexion a été réinitialisée par le serveur principal.")
+                    break
+                if not data:
+                    print("Watchdog: Connexion perdue avec le serveur principal.")
+                    break
+                elif data == b'pong':
+                    print("Watchdog: Pong reçu du serveur principal.")
+                else:
+                    print(f"Watchdog: Donnée inattendue reçue : {data}")
+                time.sleep(CHECK_INTERVAL)
 
-def receive_response_signal(signum, frame):
-    global got_response
-    got_response = True
 
 if __name__ == '__main__':
-    signal.signal(signal.SIGALRM, send_check_signal)
-    signal.signal(signal.SIGUSR2, receive_response_signal)
-
-    # Démarrer le serveur principal
-    valeur_shared = multiprocessing.Value('i', 0)
-    process_to_check = serveur_principal(valeur_shared)
-
     while True:
-        got_response = False
-        signal.alarm(CHECK_INTERVAL)
-        time.sleep(RESPONSE_TIMEOUT)
+        watchdog_server()
 
-        if not got_response:
-            print("Watchdog: Pas de réponse, redémarrage du serveur")
-            os.kill(process_to_check.pid, signal.SIGKILL)
-            process_to_check = serveur_principal(valeur_shared)
